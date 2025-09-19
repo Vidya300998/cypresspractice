fb5223156ed6ce6f48b17c3f2c06e418a38ff042
Vidya300998 <vidyalakshmi597@gmail.com>
2025-09-19 01:05:44 -0700

    Add files via upload
    recent one

--- login.js
+++ login.js
@@ -1,10 +1,11 @@
 
 import {LoginPage} from "./login_page"
 const loginpage= new LoginPage()
+let mainpage= new LoginPage()
 it('sample', function() {
     loginpage.navigate('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-        loginpage.enterUsername(Admin);
-        loginpage.enterPassword(admin123);
+        loginpage.enterUsername('Admin');
+        loginpage.enterPassword('admin123');
         loginpage.clickLogin();
-
+        mainpage.clickAdmin();
 })
--- login_page.js
+++ login_page.js
@@ -1,15 +1,23 @@
 export class LoginPage{
+
+    loginPage_username=':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input';
+    loginPage_password=':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input';
+    loginPage_loginButtom='.oxd-button';
+    mainpage_clickAdmin=':nth-child(1) > .oxd-main-menu-item > .oxd-text';
 navigate(url){
     cy.visit(url)
 }
-enterUsername(username: string){
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('username')
+enterUsername(username){
+    cy.get(this.loginPage_username).type(username)
 
 }
-enterPassword(password: string){
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('password')
+enterPassword(password){
+    cy.get(this.loginPage_password).type(password)
 }
 clickLogin(){
-    cy.get('.oxd-button').click()
+    cy.get(this.loginPage_loginButtom).click()
+}
+clickAdmin(){
+    cy.get(this.mainpage_clickAdmin).click();
 }
 }
